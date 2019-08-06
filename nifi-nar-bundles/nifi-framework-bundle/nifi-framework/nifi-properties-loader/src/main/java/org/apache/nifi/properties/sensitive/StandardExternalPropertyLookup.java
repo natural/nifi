33468649d76bc08b6883c2d8ee6138188c9d4a14
Troy Melhase <troy@troy.io>
2019-08-06 08:04:57 -0800

    NIFI-6326 JCECKS (and other) Key Store Sensitive Property Provider.

---
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-properties-loader/src/main/java/org/apache/nifi/properties/sensitive/StandardExternalPropertyLookup.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.properties.sensitive;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Slurps property values from the usual places in the usual order.
+ *
+ */
+public class StandardExternalPropertyLookup implements ExternalPropertyProvider  {
+    private final File propFile;
+
+    /**
+     * Create a {@link StandardExternalPropertyLookup} without a property file.
+     */
+    public StandardExternalPropertyLookup() {
+        this(null);
+    }
+
+    /**
+     * Create a {@link StandardExternalPropertyLookup} with the given property file.
+     *
+     * @param propertiesFilename final lookup location, or null for none.
+     */
+    public StandardExternalPropertyLookup(String propertiesFilename) {
+        this.propFile = StringUtils.isNotBlank(propertiesFilename) ? new File(propertiesFilename) : null;
+    }
+
+    /**
+     * Get a string value by name from the usual locations:  System, Environment, then Properties file.
+     *
+     * @param name the name of the value to retrieve
+     * @return the value, as a String
+     */
+    public String get(String name) {
+        return get(name, null);
+    }
+
+    /**
+     * Get a string value by name from the usual locations:  System, Environment, then Properties file.
+     *
+     * @param name the name of the value to retrieve
+     * @param missing value to return if all lookups fail
+     * @return the value, as a String
+     */
+    public String get(String name, String missing) {
+        String value;
+
+        // check system prop first
+        value = System.getProperty(name);
+        if (value != null) {
+            return value;
+        }
+
+        // check env second
+        value = System.getenv(name);
+        if (value != null) {
+            return value;
+        }
+
+        // check default prop file third
+        if (propFile != null) {
+            Properties props = new Properties();
+            try {
+                props.load(new FileInputStream(propFile));
+                value = props.getProperty(name);
+            } catch (IOException ignored) {
+            }
+        }
+
+        return value != null ? value : missing;
+    }
+}
